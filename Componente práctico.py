11d0d41f81b3ca249f950b26a201aa4c30739866
Jhonier Alxander Maquilon Miranda <jmaquilonmiranda@gmail.com>
2026-04-17 23:25:05 -0500

    Actualización 1

--- Componente práctico.py
+++ Componente práctico.py
@@ -1,8 +1,8 @@
 # Fase-4---Componente-practico---Practicas-Simuladas
 # Grupo : 263
-# Integrantes:
+# Integrantes del grupo:
 # -Jhonier Alexander Maquilon Miranada 
-# -L Luis Angel Savedra
+# -Luis Angel Savedra
 
 # importar la biblioteca de tkinter para crear la interfaz gráfica
 import tkinter as tk 
@@ -11,9 +11,10 @@ from tkinter import messagebox
 
 # defino la funcion para mostrar el mensaje de bienvenida, recibe como parametro el nombre del software
 def mostrar_mensaje(Software_FJ ):
+    # utilizo la funcion para mostrar un mensaje emergente con el nombre del software
     messagebox.showinfo("Mensaje", f"¡Hola, {Software_FJ}! Bienvenido al componente práctico de Software FJ.")
 
-# Crear la ventana principal
+# Creo la ventana principal de la apliacion y le asigno un titulo y un tamaño
 ventana = tk.Tk()   
 ventana.title("Componente Práctico - Software FJ")
 ventana.geometry("300x200")
