72acf448e6e559bee66fb334eebf78c2507eff58
Jhonier Alxander Maquilon Miranda <jmaquilonmiranda@gmail.com>
2026-04-28 23:19:41 -0500

    Actualizacion 4

--- Componente práctico.py
+++ Componente práctico.py
@@ -2,46 +2,48 @@
 # Grupo : 263
 # Integrantes:
 # Jhonier Alexander Maquilon Miranada 
-# Luis Angel Savedra
+# Luis Angel Savedra Linares
 # Tutor: Juan Pablo Arango Cardona
+# Trabajo colaborativo.
+# 12 de  mayo de 2026
 
-
-
-# importo ABC para crear clases abstractas
+# importo ABC para crear clases abstractas y abstractmethod para definir métodos que deben ser implementados por las clases que hereden de la clase base
 from abc import ABC, abstractmethod
 
-# importo datetime para manejar fechas en logs
+# importo datetime para manejar fechas en logs y facilitar el seguimiento de eventos en el sistema
 import datetime
-# defino el archivo donde se guardarán los eventos del sistema
+# defino el archivo donde se guardarán los eventos del sistema 
 LOG_FILE = "logs_software_fj.txt"
 
-# creo función para registrar eventos en el sistema
+# creo función para registrar eventos en el sistema con formato de fecha y hora para facilitar el seguimiento de acciones y errores
 def log_event(msg):
 
     # obtengo la fecha y hora actual
     time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    # abro el archivo de logs en modo escritura acumulativa
+    # abro el archivo de logs en modo escritura acumulativa y con codificación utf-8 para evitar problemas con caracteres especiales
     with open(LOG_FILE, "a", encoding="utf-8") as f:
 
-        # escribo el mensaje con la fecha
+        # escribo el mensaje con la fecha y hora en el formato definido
         f.write(f"[{time}] {msg}\n")
+        
+        
 
 # implemento excepción para errores de cliente
 class ClienteError(Exception):
     pass
 
 # implemento excepción para errores de servicio
-class ServicioError(Exception):
-    pass
-
-# implemento excepción para errores de reserva
 class ReservaError(Exception):
     pass
-# uso la clase base obligatoria para entidades del sistema 
+
 class Entidad(ABC):
 
-    # ingreso método abstracto que obliga a implementar descripción
+    # constructor general con id
+    def __init__(self, id):
+        self.id = id
+
+    # método obligatorio
     @abstractmethod
     def descripcion(self):
         pass
@@ -51,6 +53,8 @@ class Cliente(Entidad):
 
     # constructor del cliente con validaciones y manejo de errores
     def __init__(self, nombre, documento):
+        # uso documento como id único del cliente para la clase base Entidad
+        super().__init__(id=documento)  
 
         try:
             # valido nombre mínimo 3 caracteres
